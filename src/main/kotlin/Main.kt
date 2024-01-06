0d1ccf68943a83302898a1434b63baa2b4ad1172
lyvius2 <lyvius2@naver.com>
2024-01-06 14:47:33 +0900

    kotlin practice

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,39 @@
+fun main() {
+    val person: Person = Person("아무개")
+    var person2: Person?
+
+    val str: String? = null
+    println(str?.length ?:0)
+}
+
+fun startsWithA(str: String): Boolean {
+    return str.startsWith("A")
+}
+
+fun startsWithA1(str: String?): Boolean {
+    return str?.startsWith("A") ?: throw IllegalArgumentException("this is null!")
+    /*
+    if (str == null) {
+        throw IllegalArgumentException("this is null!")
+    }
+    return str.startsWith("A")
+     */
+}
+
+fun startsWithA2(str: String?): Boolean? {
+    return str?.startsWith("A")
+    /*
+    if (str == null) {
+        return null
+    }
+    return str.startsWith("A")*/
+}
+
+fun startsWithA3(str: String?): Boolean {
+    return str?.startsWith("A") ?: false
+    /*
+    if (str == null) {
+        return false
+    }
+    return str.startsWith("A")*/
+}
