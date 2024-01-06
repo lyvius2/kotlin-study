af48367f7ea849489a2dda0db413a52cef2c422d
lyvius2 <lyvius2@naver.com>
2024-01-06 17:00:57 +0900

    6강 연습

---
+++ src/main/kotlin/Repeat.kt
@@ -0,0 +1,19 @@
+fun main() {
+    val numbers = listOf(1L, 2L, 3L)
+    for (number in numbers) {
+        println(number)
+    }
+    println("----")
+    for (i in 1..3) {
+        println(i)
+    }
+    println("----")
+    for (i in 3 downTo 1) {
+        println(i)
+    }
+    println("----")
+    for (i in 1..5 step 2) {
+        println(i)
+    }
+}
+
