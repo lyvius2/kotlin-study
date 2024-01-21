84dba0b531acdbc8e766a1a999a1eb7d4ad6077d
walter.hwang <lyvius2@naver.com>
2024-01-21 21:44:34 +0900

    coroutine 취소 연습

---
+++ src/main/kotlin/coroutine/Cancel.kt
@@ -0,0 +1,38 @@
+package coroutine
+
+import kotlinx.coroutines.*
+
+fun main(): Unit = runBlocking {
+    val job = launch(Dispatchers.Default) {
+        var i = 1
+        var nextPrintTime = System.currentTimeMillis()
+        while (i <= 5) {
+            if (nextPrintTime <= System.currentTimeMillis()) {
+                printWithThread("${i++}번째 출력!")
+                nextPrintTime += 1_000L
+            }
+
+            if (!isActive) {
+                throw CancellationException()
+            }
+        }
+    }
+
+    delay(100L)
+    job.cancel()
+}
+
+fun cancelSample(): Unit = runBlocking {
+    val job1 = launch {
+        delay(1_000L)
+        printWithThread("Job 1")
+    }
+
+    val job2 = launch {
+        delay(1_000L)
+        printWithThread("Job 2")
+    }
+
+    delay(100)
+    job1.cancel()
+}
--- src/main/kotlin/coroutine/Job.kt
+++ src/main/kotlin/coroutine/Job.kt
@@ -5,6 +5,10 @@ import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
 
+fun main() {
+    controlJoin()
+}
+
 fun controlJoin(): Unit = runBlocking {
     val job1 = launch {
         delay(1_000)
