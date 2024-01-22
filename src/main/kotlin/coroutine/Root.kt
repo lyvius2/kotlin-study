726b206867382fb94429304aaf60addb5bf930cd
lyvius2 <lyvius2@naver.com>
2024-01-22 12:57:40 +0900

    코루틴 예외처리 연습

---
+++ src/main/kotlin/coroutine/Root.kt
@@ -0,0 +1,47 @@
+package coroutine
+
+import kotlinx.coroutines.*
+
+fun main(): Unit = runBlocking {
+    // 자식 코루틴의 에러가 부모에게 전파된다.
+    /*
+    val job = async {
+        throw IllegalArgumentException()
+    }*/
+
+    // 자식 코루틴의 에러를 부모에게 전파하지 않는다.
+    val job = async(SupervisorJob()) {
+        throw IllegalArgumentException()
+    }
+
+    delay(1_000L)
+}
+
+fun asyncSample(): Unit = runBlocking {
+    val job = CoroutineScope(Dispatchers.Default).async {
+        throw IllegalArgumentException()
+    }
+
+    delay(1_000L)
+    job.await()
+}
+
+fun launchSample(): Unit = runBlocking {
+    val job = CoroutineScope(Dispatchers.Default).launch {
+        throw IllegalArgumentException()
+    }
+
+    delay(1_000L)
+}
+
+fun rootSample(): Unit = runBlocking {
+    val job1 = CoroutineScope(Dispatchers.Default).launch {
+        delay(1_000L)
+        printWithThread("Job 1")
+    }
+
+    val job2 = CoroutineScope(Dispatchers.Default).launch {
+        delay(1_000L)
+        printWithThread("Job 2")
+    }
+}
