8001ebd88e798de440ba0caba10da4700df190c3
walter.hwang <lyvius2@naver.com>
2024-05-26 01:42:55 +0900

    [고급 17강] 연산자 오버로딩

--- src/main/kotlin/advanced/lec17_dsl/OperatorOverloading.kt
+++ src/main/kotlin/advanced/lec17_dsl/OperatorOverloading.kt
@@ -1,5 +1,7 @@
 package advanced.lec17_dsl
 
+import java.time.LocalDate
+
 class OperatorOverloading {
 }
 
@@ -24,4 +26,20 @@ fun main() {
     println(point.zeroPointSymmetry())
     println(-point)
     println(++point)
+
+    // 2023-01-04
+    LocalDate.of(2023, 1, 1).plusDays(3)
+
+    LocalDate.of(2023, 1, 1) + Days(3)
+
+    LocalDate.of(2023, 1, 1) + 3.d
+}
+
+data class Days(val day: Long)
+
+val Int.d: Days
+    get() = Days(this.toLong())
+
+operator fun LocalDate.plus(days: Days): LocalDate {
+    return this.plusDays(days.day)
 }
