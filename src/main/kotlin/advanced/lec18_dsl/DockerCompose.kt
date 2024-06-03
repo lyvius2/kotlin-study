140ce739f7a2ac8b492ca5d3d4fffda45cc7b48f
walter.hwang <lyvius2@naver.com>
2024-06-03 22:47:17 +0900

    [고급 18강] Kotlin DSL : yaml 파일 렌더링

--- src/main/kotlin/advanced/lec18_dsl/DockerCompose.kt
+++ src/main/kotlin/advanced/lec18_dsl/DockerCompose.kt
@@ -1,10 +1,14 @@
 package advanced.lec18_dsl
 
-import kotlin.properties.Delegates
+import kotlin.properties.ReadWriteProperty
+import kotlin.reflect.KProperty
 
 fun main() {
     val yml = dockerCompose {
         version { 3 }
+        service(name = "db") {
+            image { "mysql" }
+        }
     }
 
     println(yml.render("  "))
@@ -18,16 +22,59 @@ fun dockerCompose(init: DockerCompose.() -> Unit): DockerCompose {
 }
 
 class DockerCompose {
-    private var version: Int by Delegates.notNull()
+    private var version: Int by onceNotNull()
+    private val services = mutableListOf<Service>()
 
     fun version(init: () -> Int) {
         version = init()
     }
 
+    fun service(name: String, init: Service.() -> Unit) {
+        val service = Service(name)
+        service.init()
+        services.add(service)
+    }
+
     fun render(indent: String): String {
         val builder = StringBuilder()
         builder.appendNew("version: '$version'")
+        builder.appendNew("services:")
+        builder.appendNew(services.joinToString("\n") { it.render(indent) }.addIndent(indent, 1))
         return builder.toString()
+    }
+}
+
+class Service(
+    private val name: String
+) {
+    private var image: String by onceNotNull()
+
+    fun image(init: () -> String) {
+        image = init()
+    }
+
+    fun render(indent: String): String {
+        val builder = StringBuilder()
+        builder.appendNew("$name:")
+        builder.appendNew("image: $image", indent, 1)
+        return builder.toString()
+    }
+}
+
+fun <T> onceNotNull() = object : ReadWriteProperty<Any?, T> {
+    private var value: T? = null
+    override fun getValue(thisRef: Any?, property: KProperty<*>): T {
+        if (this.value == null) {
+            throw IllegalArgumentException("변수가 초기화되지 않았습니다.")
+        }
+        return this.value!!
+    }
+
+    override fun setValue(thisRef: Any?, property: KProperty<*>, value: T) {
+        if (this.value == null) {
+            throw IllegalArgumentException("이 변수는 한 번만 값을 초기화할 수 있습니다.")
+        }
+        this.value = value
     }
 }
 
