f3af1ede827b3c7047673a389e2fb379b95b70de
walter.hwang <lyvius2@naver.com>
2024-06-08 01:31:29 +0900

    [고급 23강] SuperTypeToken

--- src/main/kotlin/advanced/lec23/TypeSafeCage.kt
+++ src/main/kotlin/advanced/lec23/TypeSafeCage.kt
@@ -3,7 +3,9 @@ package advanced.lec23
 import advanced.lec01.Animal
 import advanced.lec01.Cage
 import advanced.lec01.Carp
+import advanced.lec01.GoldFish
 import kotlin.reflect.KClass
+import kotlin.reflect.KType
 import kotlin.reflect.cast
 
 fun main() {
@@ -16,6 +18,17 @@ fun main() {
     typeSafeCage.putOne(Carp("고등어"))
     val one: Carp = typeSafeCage.getOne(Carp::class)
     val one2 = typeSafeCage.getOne<Carp>()
+
+    val superTypeToken1 = object : SuperTypeToken<List<GoldFish>>() {}
+    val superTypeToken2 = object : SuperTypeToken<List<GoldFish>>() {}
+    val superTypeToken3 = object : SuperTypeToken<List<Carp>>() {}
+    println(superTypeToken2.equals(superTypeToken1))
+    println(superTypeToken3.equals(superTypeToken1))
+
+    val superTypeSafeCage = SuperTypeSafeCage()
+    superTypeSafeCage.putOne(superTypeToken2, listOf(GoldFish("금붕어1"), GoldFish("금붕어2")))
+    val result = superTypeSafeCage.getOne(superTypeToken2)
+    println(result)
 }
 
 class TypeSafeCage {
@@ -38,3 +51,30 @@ class TypeSafeCage {
     }
 }
 
+class SuperTypeSafeCage {
+    private val animals: MutableMap<SuperTypeToken<*>, Any> = mutableMapOf()
+
+    fun <T : Any> getOne(token: SuperTypeToken<T>): T {
+        return this.animals[token] as T
+    }
+
+    fun <T : Any> putOne(token: SuperTypeToken<T>, animal: T) {
+        animals[token] = animal
+    }
+}
+
+// SuperTypeToken을 구현한 클래스가 인스턴스화 되면 T정보를 내부 변수에 저장
+abstract class SuperTypeToken<T> {
+    val type: KType = this::class.supertypes[0].arguments[0].type!!
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+
+        other as SuperTypeToken<*>
+        return type == other.type
+    }
+
+    override fun hashCode(): Int {
+        return type.hashCode()
+    }
+}
